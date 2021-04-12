1499dc41559b44cc1dba305d1cdeb027e7f800ba
Anatoliy_Iliev <vekok12@gmail.com>
2021-04-12 16:55:45 +0300

    w5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,6 +1,18 @@
 const inputEl = document.querySelector('input')
 const titleEl = document.querySelector('#name-output')
-console.log(inputEl)
-console.log(titleEl.textContent)
-inputEl.placeholder = 'незнакомец'
-titleEl.textContent = 'xfgsdfg'
+// console.dir(inputEl)
+// console.log(inputEl)
+// console.log(titleEl.textContent)
+// console.log(inputEl.value)
+// inputEl.placeholder = 'незнакомец'
+// titleEl.textContent = 'xfgsdfg'
+
+inputEl.addEventListener('change', changeInput);
+
+function changeInput() {
+    if (inputEl.value === ''){
+        return titleEl.textContent = 'незнакомец'; 
+    }
+    // console.log(inputEl.value)
+    return titleEl.textContent = inputEl.value;    
+}
