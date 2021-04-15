263fbb53960cf5e0d914b4b366bca95a94a7da76
Anatoliy_Iliev <vekok12@gmail.com>
2021-04-15 10:47:27 +0300

    a8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,28 +1,25 @@
-
 const refs = {
     controlsDivEl: document.querySelector('#controls'),
     boxesDivEl: document.querySelector('#boxes'),
-    actionButton: document.querySelectorAll('button'),
-    inputEl: document.querySelector('input')
+    actionButton: document.querySelectorAll('button'),    
 }
 
 refs.actionButton[0].addEventListener('click', createBoxes);
 refs.actionButton[1].addEventListener('click', destroyBoxes);
-refs.inputEl.addEventListener('input', createBoxes);
+// refs.inputEl.addEventListener('input', createBoxes);
 
 function createBoxes(amount) {
-    console.log(Number(amount.currentTarget.value));
+    const inputEl = document.querySelector('input')
+    console.log(Number(inputEl.value));
     // console.log(amount.currentTarget.value);
-    // refs.boxesDivEl.insertAdjacentHTML('afterbegin', '<div>');  
-    // console.log(refs.boxesDivEl);
-    return refs.boxesDivEl * Number(amount.currentTarget.value);
-
+    // const numberOfDiv = Number(amount.currentTarget.value);
+    // console.log(Number(amount.currentTarget.value));
+    const numberOfDiv = refs.boxesDivEl.insertAdjacentHTML('afterbegin', "<div class='random-colour'></div>"); 
+    return numberOfDiv * Number(inputEl.value);
 }
 
-function destroyBoxes(){
-  
-    return refs.boxesDivEl = " " ; 
-
+function destroyBoxes(){  
+    // return refs.boxesDivEl = " " ; 
 }
 
 // refs.boxesDivEl.insertAdjacentHTML('afterbegin', '<div>');  
