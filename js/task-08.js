d981338639def877e8a6bf3ad6ac3e377840d512
AnatoliyIliev <vekok12@gmail.com>
2022-10-24 18:53:18 +0300

    feat: add clickButton

--- js/task-08.js
+++ js/task-08.js
@@ -3,18 +3,18 @@ const input = document.querySelector('input');
 const addButton = document.querySelector('[data-action="render"]');
 const delButton = document.querySelector('[data-action="destroy"]');
 
-addButton.addEventListener('click', createBoxes);
+addButton.addEventListener('click', clickButton);
 delButton.addEventListener('click', destroyBoxes);
 
 function createBoxes(amount) {
+  let arr = [];
   let currentSize = 30;
   //   let currentColor = () => Math.floor(Math.random() * 16777215).toString(16);
   let Color = () => Math.floor(Math.random() * 255);
 
-  for (let i = 0; i < input.value; i += 1) {
+  for (let i = 0; i < amount; i += 1) {
     const createDivElement = document.createElement('div');
     const currentRGBColor = () => `rgb(${Color()}, ${Color()}, ${Color()})`;
-
     createDivElement.setAttribute(
       'style',
       `background: ${currentRGBColor()}; width: ${currentSize}px; height: ${currentSize}px; margin: 5px; border: 1px solid black`,
@@ -22,11 +22,17 @@ function createBoxes(amount) {
 
     currentSize += 10;
 
-    divEl.insertAdjacentElement('beforeend', createDivElement);
+    arr.push(createDivElement);
   }
+
+  return arr;
 }
 
 function destroyBoxes() {
   divEl.innerHTML = '';
   input.value = '';
+}
+
+function clickButton() {
+  divEl.append(...createBoxes(input.value));
 }
